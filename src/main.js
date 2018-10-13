06308108ae0b8ff53ee950408d538690720787dc
Alex Shakun <alx.safari@gmail.com>
2018-10-13 12:16:42 +0300

    satisfy linter

--- src/main.js
+++ src/main.js
@@ -8,11 +8,14 @@ export default async (city = 'moscow') => {
     if (locationData[0].woeid) {
       const { data: weatherData } = await axios.get(`${weatherServiceApiUrl}/${locationData[0].woeid}/`);
 
-      return weatherData && weatherData.consolidated_weather && weatherData.consolidated_weather[0].weather_state_name;
+      return weatherData && weatherData.consolidated_weather
+      && weatherData.consolidated_weather[0].weather_state_name;
     }
 
     console.log('No such city in our service, sorry. Try again!');
   } catch (e) {
     console.log(e);
   }
+
+  return undefined;
 };
